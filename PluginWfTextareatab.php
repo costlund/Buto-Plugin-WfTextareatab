5095c0fb1940d40d8cfce0d696d5699d38219067
Claes Ö <>
2022-02-18 12:50:55 +0100

    1.0.1

--- PluginWfTextareatab.php
+++ PluginWfTextareatab.php
@@ -1,26 +1,7 @@
 <?php
-/**
-<p>
-Add tab functionality to textarea.
-</p>
- */
 class PluginWfTextareatab{
-  /**
-  <p>
-  Including js in html/head section.
-  </p>
-  #code-yml#
-  -
-    type: widget
-    data:
-      plugin: 'wf/ajax'
-      method: include
-  #code#
-  */
   public static function widget_include(){
     $element = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/wf/textareatab/PluginWfTextareatab.js'));
     wfDocument::renderElement(array($element));
   }
-  
-  
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Buto-Plugin-WfTextareatab
+Add tab functionality to textarea.
+One could replace a tab with any character.
+
+## Include
+````
+type: widget
+data:
+  plugin: 'wf/textareatab'
+  method: include
+````
+
+## Usage
+Call method like this. Second parameter replace a tab with two slashes.
+````
+PluginWfTextareatab.setTextareaTabEnabled(document.getElementById('my_textarea'), '  ');
+````
